65f28bff6bdeff0eb8d4bb5368d9cf721692339c
Lucas Vasconcelos <lucasv1992@gmail.com>
2022-09-15 13:11:58 -0300

    light dark mode incompleto

--- projects/ngx-corebox/src/lib/template/template.component.ts
+++ projects/ngx-corebox/src/lib/template/template.component.ts
@@ -13,16 +13,50 @@ export class TemplateComponent implements OnInit {
   @Input() companyName: string = 'companyName';
   @Input() menuItems: MenuItem[] = [];
   @Input() appItems: AppItem[] = [];
-  @Input() mainColor: string = '#470066';
-  @Input() secondaryColor: string = '#8a2be2';
+  @Input() darkMode: boolean = false;
+  @Input() lightModeMainColor: string = '#470066';
+  @Input() lightModeSecondaryColor: string = '#8a2be2';
+  @Input() lightModeDesktopMenuColor: string = '#f3f3f3';
+  @Input() lightModeMobileMenuColor: string = '#470066';
+  @Input() lightModeContentColor: string = '#f9f9f9';
+  @Input() lightModeDefaultTextColor: string = '#000000';
+  @Input() lightModeTextColorOnMain: string = '#ffffff';
+  @Input() lightModeTextColorOnMenu: string = '#000000';
+  
+  @Input() darkModeMainColor: string = '#470066';
+  @Input() darkModeSecondaryColor: string = '#8a2be2';
+  @Input() darkModeDesktopMenuColor: string = '#201f1e';
+  @Input() darkModeMobileMenuColor: string = '#201f1e';
+  @Input() darkModeContentColor: string = '#252423';
+  @Input() darkModeDefaultTextColor: string = '#e9f4f4';
+  @Input() darkModeTextColorOnMain: string = '#000000';
+  @Input() darkModeTextColorOnMenu: string = '#000000';
 
   menuIsOpen: boolean = false;
   appsIsOpen: boolean = false;
 
   ngOnInit(): void {
     this.menuIsOpen = !isMobile();
-    document.documentElement.style.setProperty('--main-color', this.mainColor);
-    document.documentElement.style.setProperty('--secondary-color', this.secondaryColor);
+    
+    if (this.darkMode) {
+      document.documentElement.style.setProperty('--main-color', this.darkModeMainColor);
+      document.documentElement.style.setProperty('--secondary-color', this.darkModeSecondaryColor);
+      document.documentElement.style.setProperty('--desktop-menu-color', this.darkModeDesktopMenuColor);
+      document.documentElement.style.setProperty('--mobile-menu-color', this.darkModeMobileMenuColor);
+      document.documentElement.style.setProperty('--content-color', this.darkModeContentColor);
+      document.documentElement.style.setProperty('--default-text-color', this.darkModeDefaultTextColor);
+      document.documentElement.style.setProperty('--text-color-on-main', this.darkModeTextColorOnMain);
+      document.documentElement.style.setProperty('--text-color-on-menu', this.darkModeTextColorOnMenu);
+    } else {
+      document.documentElement.style.setProperty('--main-color', this.lightModeMainColor);
+      document.documentElement.style.setProperty('--secondary-color', this.lightModeSecondaryColor);
+      document.documentElement.style.setProperty('--desktop-menu-color', this.lightModeDesktopMenuColor);
+      document.documentElement.style.setProperty('--mobile-menu-color', this.lightModeMobileMenuColor);
+      document.documentElement.style.setProperty('--content-color', this.lightModeContentColor);
+      document.documentElement.style.setProperty('--default-text-color', this.lightModeDefaultTextColor);
+      document.documentElement.style.setProperty('--text-color-on-main', this.lightModeTextColorOnMain);
+      document.documentElement.style.setProperty('--text-color-on-menu', this.lightModeTextColorOnMenu);
+    }
   }
 
   appsOpened($event: boolean): void {
