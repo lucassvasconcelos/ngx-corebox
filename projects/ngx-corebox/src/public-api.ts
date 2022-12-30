37f537267f495458c83b3aa47c3ec95bac399e05
Gilberto Ferreira <gilberto.ferreira@hinova.com.br>
2022-12-30 11:48:29 -0300

    feat: eposição de tipos públicos para o pacote

--- projects/ngx-corebox/src/public-api.ts
+++ projects/ngx-corebox/src/public-api.ts
@@ -5,7 +5,10 @@ export * from './lib/menu/menu.component';
 export * from './lib/template/template.component';
 export * from './lib/apps/apps.component';
 export * from './lib/avatar/avatar.component';
-export * from './lib/notifications/notifications.component';
 export { AppItem } from './lib/types/app-item'
 export { MenuItem } from './lib/types/menu-item'
-export { Notification } from './lib/types/notification'
+export { Theme } from './lib/types/theme';
+export { Header } from './lib/types/theme';
+export { Menu } from './lib/types/theme';
+export { Footer } from './lib/types/theme';
+export { ProfileOptions } from './lib/types/profile-options';
