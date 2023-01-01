264461a4dcd5eb1127fb908eb6d04bfedce91d0d
Lucas Vasconcelos <lucasv1992@gmail.com>
2023-01-01 06:08:14 -0300

    feat: atualiza para angular 15 e atualiza alguns componentes

--- projects/labs/src/app/app.routing.module.ts
+++ projects/labs/src/app/app.routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DoisComponent } from './dois/dois.component';
+import { UmComponent } from './um/um.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'um', pathMatch: 'full' },
+  { path: 'um', component: UmComponent },
+  { path: 'dois', component: DoisComponent }
 ];
 
 @NgModule({
---
+++ projects/labs/src/app/dois/dois.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dois',
+  templateUrl: './dois.component.html',
+  styleUrls: ['./dois.component.scss']
+})
+export class DoisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
